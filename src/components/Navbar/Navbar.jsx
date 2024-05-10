510cce325205258aa3f30795972c3169c70e84be
Pavank-1812 <148217455+Pavank-1812@users.noreply.github.com>
2024-05-10 22:30:58 +0530

    updated skills

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             <a href="#about">About</a>
           </li>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#experience">Skills</a>
           </li>
           <li>
             <a href="#projects">Projects</a>
