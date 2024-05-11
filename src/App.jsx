909c5c2115050c158c6ea96ac8221f92bf80eed1
Pavank-1812 <148217455+Pavank-1812@users.noreply.github.com>
2024-05-11 18:58:47 +0530

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import styles from "./App.module.css";
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
-import Experience from "./components/Experience/Experience"
-import { Projects } from "./components/Projects/Projects";
+import Experience from "./components/Experience/Experience" //default export
+import { Projects } from "./components/Projects/Projects"; //named export
+import Contact from "./components/Contact/Contact"; //default export
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Experience />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils.js";
+
+const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div class={styles.text}>
+        <h2>Contact Me 📞</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul class={styles.links}>
+        <li class={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email-Icon" />
+          <a target="_blank" href="mailto:pavankanamarlapudi18@gmail.com">
+            pavankanamarlapudi18@gmail.com
+          </a>
+        </li>
+        <li class={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn-Icon"
+          />
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/pavan-kanamarlapudi-124958214/"
+          >
+            LinkedIn.com/PavanK
+          </a>
+        </li>
+        <li class={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github-Icon" />
+          <a target="_blank" href="mailto:pavankanamarlapudi18@gmail.com">
+            github.com/PavanK-1812
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Contact;
